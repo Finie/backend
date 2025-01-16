68cabb01de16ceac1294e82bdbbaba529346624f
finie <finiephenius@gmail.com>
2025-01-16 06:15:38 +0300

    feat: implementation of crud ops on userprofile with h2 in memory database and JPA

--- src/main/java/com/spring/backend/controller/UserProfileController.java
+++ src/main/java/com/spring/backend/controller/UserProfileController.java
@@ -3,11 +3,9 @@ package com.spring.backend.controller;
 
 import com.spring.backend.models.UserProfile;
 import com.spring.backend.repository.UserProfileRepository;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,7 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/api/user/")
 public class UserProfileController {
-
 
     private UserProfileRepository userProfileRepository;
 
@@ -29,24 +26,62 @@ public class UserProfileController {
 
     @RequestMapping(method = RequestMethod.POST)
     public UserProfile createUser(@RequestBody UserProfile userProfile){
-
-        System.out.println("Here is the data info: "+userProfile.toString());
-
         return userProfileRepository.save(userProfile);
-
     }
 
 
 
     @RequestMapping(method = RequestMethod.GET)
     public List<UserProfile> getAllUsers(){
-
-
-        var userList =  userProfileRepository.findAll();
-
-
-        System.out.println("Here is the getAllUsers called: "+userList.toString() );
-
-        return userList;
+        return userProfileRepository.findAll();
     }
+
+
+    @RequestMapping(path = "{id}", method = RequestMethod.GET)
+    public UserProfile getUserById(@PathVariable String id){
+
+        return  userProfileRepository.findById(id).get();
+
+    }
+
+
+
+    @RequestMapping(path = "{id}", method = RequestMethod.PUT)
+    public UserProfile updateUser(@RequestBody UserProfile userProfile, @PathVariable String id){
+
+        var userAvailable = userProfileRepository.findById(id);
+
+        if (userAvailable.isPresent()){
+            var profile = userAvailable.get();
+            profile.setName(userProfile.getName());
+            profile.setType(userProfile.getType());
+            profile.setBalance(userProfile.getBalance());
+            profile.setDescription(userProfile.getDescription());
+
+            return userProfileRepository.save(profile);
+        }
+
+        return userProfile;
+    }
+
+
+    @RequestMapping(path = "{id}", method = RequestMethod.DELETE)
+    public void deleteUser(@PathVariable String id){
+
+        var userAvailable = userProfileRepository.findById(id);
+
+        userAvailable.ifPresent(
+                userProfile -> {
+                   userProfileRepository.delete(userProfile);
+                }
+        );
+
+
+    }
+
+
+
+
+
+
 }
